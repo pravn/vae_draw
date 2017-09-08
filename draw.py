9d8f198ad642eca42dfbcec0654b0612344fe9f7
Praveen Narayanan <prav.narayanan@gmail.com>
2017-09-08 15:55:54 -0700

    A version that seems to reconstruct mnist images. No cuda yet.

--- draw.py
+++ draw.py
@@ -5,9 +5,19 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torchvision import datasets, transforms
+import torch.nn.functional as F
+
+import os
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import scipy.io
+import string
+import numpy as np
+import torch
+import torch.utils.data 
 
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+parser.add_argument('--batch-size', type=int, default=1, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 2)')
@@ -37,8 +47,9 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=args.batch_size, shuffle=True, **kwargs)
 
 
+
 class draw(nn.Module):
-    def __init__(self, input_size, seq_len = 1):
+    def __init__(self, input_size, seq_len, batch_size):
         super(draw, self).__init__()
         #self.read()
         #self.encoder_RNN()
@@ -48,107 +59,140 @@ class draw(nn.Module):
         #self.write()
 
         self.input_size = input_size 
+        self.seq_len = seq_len
         self.dec_hidden_size = input_size
         self.enc_hidden_size = input_size
         self.hidden_size = input_size 
         self.z_size = int(self.hidden_size/2)
+        self.batch_size = batch_size
 
         #        self.enc_mu = nn.gru(input_size, enc_hidden_size, num_layers)
         #        self.enc_logsigma = nn.gru(input_size, enc_hidden_size, num_layers)
         #        self.write = nn.gru(dec_hidden_size, input_size, num_layers)
 
         #writer -> encoder_mu
-        self.enc_mu = nn.GRUCell(2*self.input_size, self.enc_hidden_size)
+        self.enc_mu = nn.GRUCell(3*self.input_size, self.enc_hidden_size)
         #writer -> encoder_logsigma
-        self.enc_logsigma = nn.GRUCell(2*self.input_size, self.enc_hidden_size)
+        self.enc_logvar = nn.GRUCell(3*self.input_size, self.enc_hidden_size)
         #hidden_mu->mu 
         self.mu_fc = nn.Linear(self.enc_hidden_size, self.z_size)
-        #hidden_logsigma->logsigma
-        self.logsigma_fc = nn.Linear(self.enc_hidden_size, self.z_size)
+        #hidden_logvar->logvar
+        self.logvar_fc = nn.Linear(self.enc_hidden_size, self.z_size)
         #do z = mu+\epsilon sigma in reparametrize
         #send to decoder hidden RNN 
 
         self.dec_rnn = nn.GRUCell(self.z_size, self.dec_hidden_size)
         self.write   = nn.Linear(self.dec_hidden_size, self.input_size)
+
+        #self.enc_input = Variable(torch.zeros(self.seq_len, self.batch_size, 3*self.input_size))
+        
+                
         
 
-        def read(x, x_hat):
-            return torch.cat((x, x_hat), -1)
+    def read(self, x, x_hat):
+        #print('x.size()', x.size())
+        #print('x_hat.size()', x_hat.size())
+        #print('read done')
+        #print('---------')
+        return torch.cat((x, x_hat), -1)
 
-        def encoder_RNN(r, seq_id):
-            #2 RNNs, one for mu, one for logsigma 
-            '''
-            #formulas from paper without attention
-            enc_input = torch.cat(r, h_dec) #skip connection from decoder
-            mu, self.h_mu = self.enc_mu(x, self.h_mu)
-            self.mu = nn.ReLU(mu)
-            logsigma, self.h_logsigma = self.enc_logsigma(x, self.h_logsigma)
-            logsigma = nn.tanh(logsigma)
-            return mu, logsigma'''
+    def encoder_RNN(self, r, h_mu_prev, h_logvar_prev, h_dec_prev, seq_id):
+        #2 RNNs, one for mu, one for logvar 
+        #formulas from paper without attention
+        '''
+        enc_input = torch.cat(r, h_dec) #skip connection from decoder
+        mu, self.h_mu = self.enc_mu(x, self.h_mu)
+        self.mu = nn.ReLU(mu)
+        logvar, self.h_logvar = self.enc_logsigma(x, self.h_logvar)
+        logsigma = nn.tanh(logvar)
+        return mu, logvar'''
+        
+        '''
+        if(seq_id==0):
+            h_dec_prev = Variable(torch.zeros(self.batch_size, self.input_size))
+        else:
+            h_dec_prev = self.h_dec[seq_id-1]'''
 
-            if(seq_id==0):
-                h_mu_prev = torch.zeros(self.h_mu.size(1), self.h_mu.size(2))
-                h_logsigma_prev = torch.zeros(self.h_logsigma.size(1), self.h_logsigma.size(2))
-                h_dec = torch.zeros(h_dec.size(1), h_dec.size(2))
-            else:
-                h_mu_prev = self.h_mu[seq_id-1]
-                h_logsigma_prev = self.h_logsigma[seq_id-1]
-                h_dec_prev = self.h_dec[seq_id-1]
+        enc_input = torch.cat((r, h_dec_prev), -1) #skip connection from decoder 
+
+        h_mu = self.enc_mu(enc_input, h_mu_prev)
+        mu = F.relu(self.mu_fc(h_mu))
+        h_logvar = self.enc_logvar(enc_input, h_logvar_prev)
+        logvar = F.relu(self.logvar_fc(h_logvar))
+
+        #print("encoder done")
+        #print("------------")
+
+        return mu, h_mu, logvar, h_logvar
+ 
+
+    def decoder_network(self, z, h_dec_prev):
+        h_dec = self.dec_rnn(z, h_dec_prev)
+        c = F.sigmoid(self.write(h_dec))
+        #print("decoder done")
+        #print("------------")
+
+        return c, h_dec
             
-            enc_input = torch.cat((r, h_dec_prev), -1) #skip connection from decoder 
-
-            self.h_mu[seq_id] = self.enc_mu(enc_input, h_mu_prev)
-            mu = nn.ReLu(self.mu_fc(self.h_mu[seq_id]))
-
-            self.h_logsigma[seq_id] = self.enc_sigma(enc_input, h_logsigma_prev)
-            logsigma = nn.ReLu(self.sigma_fc(self.h_logsigma[seq_id]))
             
-            return mu, logsigma
+    def reparametrize_and_sample(self, mu, logvar):
+        std = logvar.mul(0.5).exp_() 
 
-        def decoder_network(self, z, seq_id):
-            if(seq_id==0):
-                h_dec_prev = torch.zeros(self.decoder_hidden_size)
-            else:
-                h_dec_prev = h_dec[seq_id-1]
+        #sampling epsilon
+        if args.cuda:
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        
+        #mu + sigma * epsilon
+        #print("reparametrize and sample done")
+        #print("-----------------------------")
 
-            self.h_dec[seq_id] = dec_rnn(z, h_dec_prev)
-            c = nn.Sigmoid(write(self.h_dec[seq_id]))
-            return c
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x_in, T):
+        #advance by T timesteps 
+        x = x_in.view(-1, input_size) #flatten
+        x_t_prev = Variable(torch.zeros(self.batch_size, self.hidden_size))
+        h_mu = Variable(torch.zeros(self.batch_size, self.hidden_size))
+        h_logvar = Variable(torch.zeros(self.batch_size, self.hidden_size))
+        mu = Variable(torch.zeros(self.batch_size, self.hidden_size))
+        logvar = Variable(torch.zeros(self.batch_size, self.hidden_size))
+        h_dec = Variable(torch.zeros(self.batch_size, self.hidden_size))
+
+        mu_t = []
+        logvar_t = []
             
-            
-        def reparametrize_and_sample(self, mu, logsigma):
-            std = logsigma.mul(0.5).exp_()
+        for seq in range(T):
+            x_hat = x - F.sigmoid(x_t_prev)
+            r = self.read(x, x_hat) #cat operation
+            mu, h_mu, logvar, h_logvar = self.encoder_RNN(r, h_mu, h_logvar, h_dec, seq)
+            z = self.reparametrize_and_sample(mu, logvar)
+            c, h_dec = self.decoder_network(z, h_dec)
+            mu_t.append(mu)
+            logvar_t.append(logvar)
+            x_t_prev = c
 
-            if args.cuda:
-                eps = torch.cuda.FloatTensor(std.size()).normal_()
-            else:
-                eps = torch.FloatTensor(std.size()).normal_()
-            eps = Variable(eps)
-            return eps.mul(std).add_(mu)
+        #print("FORWARD PASS DONE")
+        #print("=================")
 
-        def forward(self, x, T):
-            #advance by T timesteps 
-            mu_t = []
-            logsigma_t = []
+        return c, mu_t, logvar_t
 
-            x_t_prev = Variable(torch.zeros(x.size(1), x.size(2)))
-            
-            for seq in range(T):
-                x_hat = x - nn.Sigmoid(x_t_prev)
-                r = self.read(x, x_hat) #cat operation
-                mu, logsigma = self.encoder_RNN(r, seq) 
-                z = self.reparametrize(mu, logsigma)
-                c = self.decoder_network(z, seq)
-                mu_t.append(mu)
-                logsigma_t.append(logsigma)
-                x_t_prev = c
-            
-            return c, mu_t, logsigma_t
 
+'''
+class draw(nn.Module):
+    def __init__(self, input_size, seq_len):
+        super(draw, self).__init__()
+        
+    def forward(self, x, seq_len):
+        for seq in range(seq_len):
+            print(seq) '''
 
 input_size = 784
 seq_len = 2
-model = draw(input_size, seq_len)
+batch_size = 128
+model = draw(input_size, seq_len, batch_size)
 if args.cuda:
     model.cuda()
 
@@ -180,21 +224,80 @@ def loss_function(recon_x, x, mu, logvar, T):
 
     return BCE + KLD 
 
-
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 def train(epoch, T):
     model.train()
     train_loss = 0
     for batch_idx, (data, _) in enumerate(train_loader):
-        data = Variable(data)
+        h_data = Variable(data)
+        #print('data.size()', data.size())
+        data = h_data.squeeze(0)
+        #print('squeezed data.size()', data.size())
         if args.cuda:
             data = data.cuda()
         optimizer.zero_grad()
-        #mu_t, logsigma_t are arrays
-        recon_batch, mu_t, logsigma_t = model(data, T)
 
-        loss = loss_function(recon_batch, data, mu_t, logsigma_t, T)
+        recon_batch, mu_t, logvar_t = model(data, T)
+        loss = loss_function(recon_batch, data, mu_t, logvar_t, T)
+        loss.backward()
+
+        train_loss += loss.data[0]
+        optimizer.step()
+
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.data[0] / len(data)))
+
+
+
+            #download to host             
+            samples = recon_batch.data.cpu().numpy()[:16]
+            
+            fig = plt.figure(figsize=(4,4))
+            gs  = gridspec.GridSpec(4,4)
+            gs.update(wspace=0.05, hspace=0.05)
+
+
+            for i, sample in enumerate(samples):
+                ax = plt.subplot(gs[i])
+                plt.axis('off')
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_aspect('equal')
+                plt.imshow(sample.reshape(28,28), cmap='gray')
+
+            if not os.path.exists('out/'):
+                os.makedirs('out/')
+            
+            plt.savefig('out/snapshot.png', bbox_inches='tight')
+            plt.close(fig)
+
+            samples = data.data.numpy()[:16]
+            fig = plt.figure(figsize=(4,4))
+            gs  = gridspec.GridSpec(4,4)
+            gs.update(wspace=0.05, hspace=0.05)
+
+
+            for i, sample in enumerate(samples):
+                ax = plt.subplot(gs[i])
+                plt.axis('off')
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_aspect('equal')
+                plt.imshow(sample.reshape(28,28), cmap='gray')
+
+            if not os.path.exists('out/'):
+                os.makedirs('out/')
+            
+            plt.savefig('out/snapshot_o.png', bbox_inches='tight')
+            plt.close(fig)
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
 
 
 for epoch in range(1):
